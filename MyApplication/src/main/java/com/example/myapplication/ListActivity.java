b9c23436140f2d9ffeb3dabaabee50d3d8c2ec10
Peter Lee <peterjlee@guidebook.com>
2013-07-16 03:36:15 -0700

    sherlock now works like choice multiple modal

--- MyApplication/src/main/java/com/example/myapplication/ListActivity.java
+++ MyApplication/src/main/java/com/example/myapplication/ListActivity.java
@@ -1,22 +1,31 @@
 package com.example.myapplication;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.ActionMode;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by peterjlee on 7/14/13.
  */
-public class ListActivity extends Activity {
+public class ListActivity extends SherlockActivity implements AdapterView.OnItemLongClickListener {
 
     private Context context;
     private ListView listView;
     private ListAdapterSample adapter;
+    private ActionMode mMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,18 @@ public class ListActivity extends Activity {
         context = this.getApplicationContext();
         adapter = new ListAdapterSample(context, 25);
         listView.setAdapter(adapter);
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-        listView.setMultiChoiceModeListener(new ListMultiListener(this));
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        //listView.setItemsCanFocus(false);
+        listView.setOnItemLongClickListener(this);
+        //listView.setMultiChoiceModeListener(new ListMultiListener(this));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mMode == null) {
+                    finish();
+                }
+            }
+        });
     }
 
     public void deleteSelectedItems(List list) {
@@ -37,6 +56,79 @@ public class ListActivity extends Activity {
 
     public void option2Selected() {
         Toast.makeText(context, "option 2 selected", Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        // Notice how the ListView api is lame
+        // You can use mListView.getCheckedItemIds() if the adapter
+        // has stable ids, e.g you're using a CursorAdaptor
+        if (mMode == null) {
+            mMode = startActionMode(new ModeCallback());
+            listView.setItemChecked(position, true);
+        }
+
+        return true;
+    };
+
+    private final class ModeCallback implements ActionMode.Callback {
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            // Create the menu from the xml file
+            MenuInflater inflater = getSupportMenuInflater();
+            inflater.inflate(R.menu.menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            // Here, you can checked selected items to adapt available actions
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            // Destroying action mode, let's unselect all items
+            for (int i = 0; i < listView.getAdapter().getCount(); i++)
+                listView.setItemChecked(i, false);
+
+            if (mode == mMode) {
+                mMode = null;
+            }
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.option1:
+                    deleteSelected();
+                    mode.finish();
+                    return true;
+                case R.id.option2:
+                    finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    };
+
+    public void deleteSelected() {
+        SparseBooleanArray checked = listView.getCheckedItemPositions();
+        int numChecked = 0;
+        ArrayList<Integer> checkedPos = new ArrayList<Integer>();
+        String checkedNums = "";
+        for (int i = 0; i < checked.size(); i++) {
+            if(checked.valueAt(i)) {
+                numChecked++;
+                checkedPos.add(checked.keyAt(i));
+                checkedNums = checkedNums + ", " + checked.keyAt(i);
+            }
+        }
+        java.util.Collections.sort(checkedPos);
+
+        adapter.deleteCheckedItems(checkedPos);
+        Toast.makeText(this, "#checked: " + numChecked + " at: " + checkedNums, Toast.LENGTH_LONG).show();
     }
 }
--- MyApplication/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends SherlockActivity {
 
     public Button button;
     public Context context;
-    private ActionMode mActionMode;
+    //private ActionMode mActionMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +41,12 @@ public class MainActivity extends SherlockActivity {
         button.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                if (mActionMode != null) {
-                    return false;
-                }
+//                if (mActionMode != null) {
+//                    return false;
+//                }
 
                 // Start the CAB using the ActionMode.Callback defined above
-                mActionMode = MainActivity.this.startActionMode(mActionModeCallback);
+                //mActionMode = MainActivity.this.startActionMode(mActionModeCallback);
                 view.setSelected(true);
                 return true;
             }
@@ -54,74 +54,44 @@ public class MainActivity extends SherlockActivity {
     }
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public void onCreateContextMenu(ContextMenu menu, View v,
-//                                    ContextMenu.ContextMenuInfo menuInfo) {
-//        super.onCreateContextMenu(menu, v, menuInfo);
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu, menu);
-//    }
-
-//    @Override
-//    public boolean onContextItemSelected(MenuItem item) {
-//        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-//        switch (item.getItemId()) {
-//            case R.id.option1:
-//                Toast.makeText(context, "Option1!", Toast.LENGTH_LONG).show();
-//                return true;
-//            case R.id.option2:
-//                Toast.makeText(context, "2222!", Toast.LENGTH_LONG).show();
-//                return true;
-//            default:
-//                return super.onContextItemSelected(item);
+//    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+//
+//        // Called when the action mode is created; startActionMode() was called
+//        @Override
+//        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+//            // Inflate a menu resource providing context menu items
+//            MenuInflater inflater = mode.getMenuInflater();
+//            inflater.inflate(R.menu.menu, menu);
+//            return true;
 //        }
-//    }
-
-    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
-
-        // Called when the action mode is created; startActionMode() was called
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            // Inflate a menu resource providing context menu items
-            MenuInflater inflater = mode.getMenuInflater();
-            inflater.inflate(R.menu.menu, menu);
-            return true;
-        }
-
-        // Called each time the action mode is shown. Always called after onCreateActionMode, but
-        // may be called multiple times if the mode is invalidated.
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            return false; // Return false if nothing is done
-        }
-
-        // Called when the user selects a contextual menu item
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.option1:
-                    Toast.makeText(context, "Option1!", Toast.LENGTH_LONG).show();
-                    mode.finish();
-                    return true;
-                case R.id.option2:
-                    Toast.makeText(context, "2222!", Toast.LENGTH_LONG).show();
-                    mode.finish();
-                default:
-                    return false;
-            }
-        }
-
-        // Called when the user exits the action mode
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            mActionMode = null;
-        }
-    };
+//
+//        // Called each time the action mode is shown. Always called after onCreateActionMode, but
+//        // may be called multiple times if the mode is invalidated.
+//        @Override
+//        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+//            return false; // Return false if nothing is done
+//        }
+//
+//        // Called when the user selects a contextual menu item
+//        @Override
+//        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.option1:
+//                    Toast.makeText(context, "Option1!", Toast.LENGTH_LONG).show();
+//                    mode.finish();
+//                    return true;
+//                case R.id.option2:
+//                    Toast.makeText(context, "2222!", Toast.LENGTH_LONG).show();
+//                    mode.finish();
+//                default:
+//                    return false;
+//            }
+//        }
+//
+//        // Called when the user exits the action mode
+//        @Override
+//        public void onDestroyActionMode(ActionMode mode) {
+//            mActionMode = null;
+//        }
+//    };
 }
